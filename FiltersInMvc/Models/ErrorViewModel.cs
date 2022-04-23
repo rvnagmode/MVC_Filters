dc23a5cf35b5aa3a474de40de2b2f294b59bf97b
rvnagmode <101249215+rvnagmode@users.noreply.github.com>
2022-04-23 12:18:30 +0530

    Add project files.

---
+++ FiltersInMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FiltersInMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FiltersInMvc/Models/TrackExecutionTime.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+namespace FiltersInMvc.Models
+{
+    public class TrackExecutionTime : Attribute, IActionFilter, IResultFilter
+    {
+        public void GetMessage(string msg)
+        {
+            File.AppendAllText(Path.GetFullPath(@"D:\vs\FiltersInMvc\Test.txt"), msg);
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            string msg = "\nOn action executed ->" + DateTime.Now.ToString()+"\n";
+            GetMessage(msg);
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            string msg = "\nOn action executing ->" + DateTime.Now.ToString() + "\n";
+            GetMessage(msg);
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            string msg = "\n On result executed->" + context.RouteData.Values["controller"].ToString() 
+                + "->" + context.RouteData.Values["action"].ToString() + 
+                " " + DateTime.Now.ToString() + "\n";
+            GetMessage(msg);
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            string msg = "\n On result executing->" + context.RouteData.Values["controller"].ToString() +
+                "->" + context.RouteData.Values["action"].ToString() + 
+                 " " + DateTime.Now.ToString() + "\n";
+            GetMessage(msg);
+        }
+    }
+}
